691119dabe417dd6f1a42d1202eb2e2f4b7c7ec5
Poncelet Grégoire <gregoire.poncelet@neuroglia.io>
2022-10-03 17:38:35 +0200

    Add project files.

---
+++ src/Example.Service/Services/ValuesService.cs
@@ -0,0 +1,9 @@
+﻿namespace Example.Service.Services
+{
+    public class ValuesService
+    {
+        public IEnumerable<string> GetValues() => new string[] { "Value1", "Value2", "Value3" };
+
+        public string GetValue() => GetValues().First();
+    }
+}
