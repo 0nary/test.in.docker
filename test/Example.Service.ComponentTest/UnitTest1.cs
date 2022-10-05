8748eaab6c2d99c947a566343b223ab2f867be10
Poncelet Grégoire <gregoire.poncelet@neuroglia.io>
2022-10-05 18:00:32 +0200

    tes

--- test/Example.Service.ComponentTest/UnitTest1.cs
+++ test/Example.Service.ComponentTest/UnitTest1.cs
@@ -19,6 +19,12 @@ public class Tests
     }
 
     [Test]
+    public void Test2()
+    {
+        Assert.Fail();
+    }
+
+    [Test]
     public async Task ShouldReturnAListOfStaticValuesOnGetRequest()
     {
         // Arrange
